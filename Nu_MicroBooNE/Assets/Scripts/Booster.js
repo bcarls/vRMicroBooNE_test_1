05a61260ab7f5b5ffd08d95fe3a3d6a7f22dc990
Alistair McLean <amclean@nmsu.edu>
2014-07-14 10:50:59 -0500

    first upload of test

---
+++ Nu_MicroBooNE/Assets/Scripts/Booster.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+
+
+var projectile : Rigidbody;
+var speed : float = 40;
+var force : float = 4;
+
+function Update () {
+    // Put this in your update function
+    if (Input.GetKeyDown(KeyCode.F)) {
+ 
+    // Instantiate the projectile at the position and rotation of this transform
+    var nu1 : Rigidbody;
+    var nu2 : Rigidbody; 
+    nu1 = Instantiate(projectile, transform.position, transform.rotation);
+ 
+    // Add force to the cloned object in the object's forward direction
+    nu1.velocity = transform.TransformDirection(Vector3(0,0,speed));
+
+
+	
+	Destroy(nu1.gameObject, 3);
+    }
+}
